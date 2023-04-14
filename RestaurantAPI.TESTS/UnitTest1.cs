555a993776de3ec251dea21658b1506475ab93a7
darek.filar <darek.filar@gmail.com>
2023-04-14 12:12:47 +0200

    Added test project

---
+++ RestaurantAPI.TESTS/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RestaurantAPI.TESTS
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
